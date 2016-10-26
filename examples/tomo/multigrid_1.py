a67a9fdae33f0d645d3a28a21b640cafacbc8d7b
Holger Kohr <h.kohr@cwi.nl>
2016-10-26 18:52:09 +0200

    WIP: add some stuff to multigrid example 1

--- examples/tomo/multigrid_1.py
+++ examples/tomo/multigrid_1.py
@@ -70,6 +70,7 @@ pspace_ray_trafo = odl.ReductionOperator(masked_ray_trafo_coarse,
 pspace = pspace_ray_trafo.domain
 phantom_c = odl.phantom.shepp_logan(coarse_discr, modified=True)
 phantom_f = odl.phantom.shepp_logan(fine_discr, modified=True)
+phantom = pspace.element([phantom_c, phantom_f])
 
 data = pspace_ray_trafo([phantom_c, phantom_f])
 data.show('data')
@@ -79,6 +80,8 @@ noisy_data.show('noisy data')
 
 reco = pspace_ray_trafo.domain.zero()
 
+# %%
+# CG reconstruction
 callback = odl.solvers.CallbackShow(display_step=5)
 odl.solvers.conjugate_gradient_normal(pspace_ray_trafo, reco, data, niter=20,
                                       callback=callback)
@@ -92,9 +95,10 @@ data_func = odl.solvers.L2NormSquared(
 reg_param_1 = 7e-3
 reg_func_1 = reg_param_1 * (odl.solvers.L2NormSquared(coarse_discr) *
                             odl.ComponentProjection(pspace, 0))
+smooth_func = data_func + reg_func_1
 
 # Non-differentiable part composed with linear operators
-reg_param = 7e-3
+reg_param = 7e-4
 nonsmooth_func = reg_param * odl.solvers.L1Norm(fine_grad.range)
 
 # Assemble into lists (arbitrary number can be given)
@@ -102,3 +106,41 @@ comp_proj_1 = odl.ComponentProjection(pspace, 1)
 lin_ops = [fine_grad * comp_proj_1]
 nonsmooth_funcs = [nonsmooth_func]
 
+# TODO: add to nonsmooth_funcs
+box_constr = odl.solvers.IndicatorBox(fine_discr,
+                                      np.min(phantom_f), np.max(phantom_f))
+f = odl.solvers.ZeroFunctional(pspace)
+
+# %%
+# eta^-1 is the Lipschitz constant of the smooth functional gradient
+ray_trafo_norm = 1.1 * odl.power_method_opnorm(pspace_ray_trafo,
+                                               xstart=phantom, maxiter=2)
+print('norm of the ray transform: {}'.format(ray_trafo_norm))
+eta = 1 / (2 * ray_trafo_norm ** 2 + 2 * reg_param_1)
+print('eta = {}'.format(eta))
+grad_norm = 1.1 * odl.power_method_opnorm(fine_grad, xstart=phantom_f,
+                                          maxiter=4)
+print('norm of the gradient: {}'.format(grad_norm))
+
+# %%
+# tau and sigma are like step sizes
+sigma = 5e-3
+tau = 2 * sigma
+# Here we check the convergence criterion for the forward-backward solver
+# 1. This is required such that the square root is well-defined
+print('tau * sigma * grad_norm ** 2 = {}, should be <= 1'
+      ''.format(tau * sigma * grad_norm ** 2))
+assert tau * sigma * grad_norm ** 2 <= 1
+# 2. This is the actual convergence criterion
+check_value = (2 * eta * min(1 / tau, 1 / sigma) *
+               np.sqrt(1 - tau * sigma * grad_norm ** 2))
+print('check_value = {}, must be > 1 for convergence'.format(check_value))
+convergence_criterion = check_value > 1
+assert convergence_criterion
+# %%
+callback = odl.solvers.CallbackShow(display_step=2)
+x = pspace.zero()  # starting point
+odl.solvers.forward_backward_pd(x, f=f, g=nonsmooth_funcs, L=lin_ops,
+                                h=smooth_func,
+                                tau=tau, sigma=[sigma], niter=60,
+                                callback=callback)
