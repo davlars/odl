8d4c921c565fae8108c1824bb5e042795060026a
Holger Kohr <h.kohr@cwi.nl>
2016-10-28 12:33:00 +0200

    WIP: gave up on gradient, need proper helpers

--- examples/tomo/multigrid_gradient.py
+++ examples/tomo/multigrid_gradient.py
@@ -69,98 +69,126 @@ class MultiGridGradient(odl.DiagonalOperator):
         # TODO: generalize to non-matching overlaps
         # TODO: handle corner cases of small inserts etc.
         # TODO: clean up notation regarding min<->left, max<->right
+        # TODO: shorten names without losing clarity
 
-        fine_arr = fine_func.asarray()
+        # TODO: turn into proper error messages & put checks into __init__
+        assert isinstance(coarse_func, odl.DiscreteLpElement)
+        assert isinstance(fine_func, odl.DiscreteLpElement)
+        assert coarse_func.ndim == fine_func.ndim
+        assert np.all(coarse_func.space.min_pt <= fine_func.space.min_pt)
+        assert np.all(coarse_func.space.max_pt >= fine_func.space.max_pt)
+
+        ndim = self.coarse_discr.ndim
 
         coarse_cell_sides = self.coarse_discr.cell_sides
+        coarse_shape = self.coarse_discr.shape
         fine_cell_sides = self.fine_discr.cell_sides
-        xmin, xmax = self.coarse_discr.min_pt, self.coarse_discr.max_pt
-        ymin, ymax = self.fine_discr.min_pt, self.fine_discr.max_pt
+        yleft, yright = self.fine_discr.min_pt, self.fine_discr.max_pt
 
-        ymin_idcs = self.coarse_discr.index(ymin)
-        ymin_idcs_f = self.coarse_discr.index(ymin, floating=True)
-        ymax_idcs = self.coarse_discr.index(ymax)
-        ymax_idcs_f = self.coarse_discr.index(ymax, floating=True)
+        # Get indices of the minimum and maximum coordinates of the insert
+        yleft_idcs = self.coarse_discr.index(yleft)
+        yleft_idcs_f = self.coarse_discr.index(yleft, floating=True)
+        yright_idcs = self.coarse_discr.index(yright)
+        yright_idcs_f = self.coarse_discr.index(yright, floating=True)
 
-        print('ymin:', ymin)
-        print('ymin_idcs:', ymin_idcs)
-        print('ymin_idcs_f:', ymin_idcs_f)
-        print('ymax:', ymax)
-        print('ymax_idcs:', ymax_idcs)
-        print('ymax_idcs_f:', ymax_idcs_f)
+        # TODO: relax this restriction
+        assert np.all(yleft_idcs > 0)
+        assert np.all(yleft_idcs < np.array(coarse_shape) - 1)
+        assert np.all(yright_idcs > 0)
+        assert np.all(yright_idcs < np.array(coarse_shape) - 1)
 
-        coarse_cvecs = self.coarse_discr.partition.coord_vectors
-        ymin_after = tuple(cvec[i + 1]
-                           for cvec, i in zip(coarse_cvecs, ymin_idcs))
-        ymin_before = tuple(cvec[i]
-                            for cvec, i in zip(coarse_cvecs, ymin_idcs))
-        ymax_before = tuple(cvec[i - 1]
-                            for cvec, i in zip(coarse_cvecs, ymax_idcs))
-        ymax_after = tuple(cvec[i]
-                           for cvec, i in zip(coarse_cvecs, ymax_idcs))
+        print('yleft:', yleft)
+        print('yleft_idcs:', yleft_idcs)
+        print('yleft_idcs_f:', yleft_idcs_f)
+        print('yright:', yright)
+        print('yright_idcs:', yright_idcs)
+        print('yright_idcs_f:', yright_idcs_f)
 
-        print('ymin_before:', np.array(ymin_before))
-        print('ymin_after:', np.array(ymin_after))
-        print('ymax_before:', np.array(ymax_before))
-        print('ymax_after:', np.array(ymax_after))
+        # TODO: handle case of first/last grid points
+        # Right now we're protected by the index condition further up
+        coarse_gvecs = self.coarse_discr.grid.coord_vectors
+        left_gridpt = [gvec[li]
+                       for gvec, li in zip(coarse_gvecs, yleft_idcs)]
+        left_gridpt_before = [gvec[li - 1]
+                              for gvec, li in zip(coarse_gvecs, yleft_idcs)]
+        right_gridpt = [gvec[ri]
+                        for gvec, ri in zip(coarse_gvecs, yright_idcs)]
+        right_gridpt_after = [gvec[ri + 1]
+                              for gvec, ri in zip(coarse_gvecs, yright_idcs)]
+
+        print('left_gridpt:', np.array(left_gridpt))
+        print('left_gridpt_before:', np.array(left_gridpt_before))
+        print('right_gridpt:', np.array(right_gridpt))
+        print('right_gridpt_after:', np.array(right_gridpt_after))
+
+        left_fine_parts, right_fine_parts = [], []
+        fine_meshgrid = fine_discr.meshgrid
+        broadcast_slice = [None] * ndim
+        for i in range(ndim):
+            bcast_slc = list(broadcast_slice)
+            bcast_slc[i] = slice(None)
+
+            # Construct meshgrids with thickness 1 in axis i and interpolate
+            # the finely sampled function there.
+
+            # For fine discr, "left before" and "right after"
+            fine_mg = list(fine_meshgrid)
+            fine_mg[i] = np.atleast_1d(left_gridpt_before[i])[bcast_slc]
+            left_fine_parts.append(fine_func.interpolation(fine_mg))
+            print('left interp:', left_fine_parts[i])
+
+            fine_mg[i] = np.atleast_1d(right_gridpt_after[i])[bcast_slc]
+            right_fine_parts.append(fine_func.interpolation(fine_mg))
+            print('right interp:', right_fine_parts[i])
+
+        # Compute the contribution of the fine part overlapping with the
+        # interval into which the fine min and max points fall. We first
+        # compute the averaged arrays, then create auxiliary spaces with 2
+        # intervals in the respective axis, and finally interpolate between
+        # the last "regular" value on the coarse grid and the averaged fine
+        # values.
 
         # TODO: take one extra if available
-        ymin_after_fine_idx = self.fine_discr.index(ymin_after)
-        ymax_before_fine_idx = self.fine_discr.index(ymax_before)
+        coarse_cvecs = self.coarse_discr.partition.coord_vectors
+        left_cell_bdry_after = [
+            cvec[li + 1] for cvec, li in zip(coarse_cvecs, yleft_idcs)]
+        left_cell_bdry_after_fine_idcs = self.fine_discr.index(
+            left_cell_bdry_after)
+        right_cell_bdry_before = [
+            cvec[ri - 1] for cvec, ri in zip(coarse_cvecs, yright_idcs)]
+        right_cell_bdry_before_fine_idcs = self.fine_discr.index(
+            right_cell_bdry_before)
 
-        print('ymin_after_fine_idx:', ymin_after_fine_idx)
-        print('ymax_before_fine_idx:', ymax_before_fine_idx)
+        print('left_cell_bdry_after:', left_cell_bdry_after)
+        print('right_cell_bdry_before:', right_cell_bdry_before)
+        print('left_cell_bdry_after_fine_idcs:',
+              left_cell_bdry_after_fine_idcs)
+        print('right_cell_bdry_before_fine_idcs:',
+              right_cell_bdry_before_fine_idcs)
 
-        # Compute the mean arrays along all axes left and right
-        left_avg_arrays, right_avg_arrays = [], []
-        ndim = self.coarse_discr.ndim
-        # TODO: perhaps better to make cut-out spaces and use integration
-        # there
+        # Compute the mean arrays along all axes, left and right, along with
+        # their weights
+        left_mean_arrays, right_mean_arrays = [], []
+        left_mean_weights, right_mean_weights = [], []
+        take_all_slice = [slice(None)] * ndim
+        fine_arr = fine_func.asarray()
         for i in range(ndim):
-            slc_l = [slice(None)] * ndim
-            slc_l[i] = slice(ymin_after_fine_idx[i])
-            left_avg_arrays.append(np.mean(fine_arr[slc_l], axis=i))
-            print(left_avg_arrays[-1])
+            slc_l = list(take_all_slice)
+            slc_l[i] = slice(left_cell_bdry_after_fine_idcs[i])
+            left_mean_arrays.append(np.mean(fine_arr[slc_l], axis=i))
+            print(left_mean_arrays[i])
 
-            slc_r = [slice(None)] * ndim
-            slc_r[i] = slice(ymax_before_fine_idx[i], None)
-            right_avg_arrays.append(np.mean(fine_arr[slc_r], axis=i))
-            print(right_avg_arrays[-1])
+            slc_r = list(take_all_slice)
+            slc_r[i] = slice(right_cell_bdry_before_fine_idcs[i])
+            right_mean_arrays.append(np.mean(fine_arr[slc_r], axis=i))
+            print(right_mean_arrays[i])
 
-        # TODO: The lambda stuff should be handled by the coarse resampling,
-        # remove if not needed.
-        left_lambda, left_coarse_idcs = [], []
-        for yi, yi_f in zip(ymin_idcs, ymin_idcs_f):
-            if np.isclose(yi, yi_f):
-                # Very close to a grid point. We take the whole cell at
-                # 1 index to the left, with weight 1.
-                left_lambda.append(yi_f - yi + 1)
-                left_coarse_idcs.append(yi - 1)
-            else:
-                # Somewhere in between the cell boundaries, just take the
-                # values as-is.
-                left_lambda.append(yi_f - yi)
-                left_coarse_idcs.append(yi)
-        print('left_lambda:', left_lambda)
-        print('coarse_min_indcs:', left_coarse_idcs)
+        # In each axis and for left/right, create auxiliary coarse spaces
+        # with size 2 in the respective axis. Create an element from the
+        # last available "real" values and the averaged fine values.
+        for i in range(ndim):
 
-        right_lambda, right_coarse_idcs = [], []
-        for yi, yi_f in zip(ymax_idcs, ymax_idcs_f):
-            if np.isclose(yi, yi_f):
-                # Very close to a grid point. We take the whole cell at
-                # 1 index to the right, with weight 1.
-                right_lambda.append(yi_f - yi + 1)
-                right_coarse_idcs.append(yi + 1)
-            else:
-                # Somewhere in between the cell boundaries, just take the
-                # values as-is.
-                right_lambda.append(yi_f - yi)
-                right_coarse_idcs.append(yi)
-        print('right_lambda:', right_lambda)
-        print('coarse_max_indcs:', right_coarse_idcs)
 
-        # TODO: do both left and right parts with interpolation!!!
-        left_fine_parts, right_fine_parts = [], []
         left_resampled_parts, right_resampled_parts = [], []
         fine_meshgrid = fine_discr.meshgrid
         for i in range(ndim):
@@ -170,7 +198,7 @@ class MultiGridGradient(odl.DiagonalOperator):
 
             # Get the views into the fine array with thickness 1 in axis
             # i, left and right. Append to the respective lists.
-            left_fine_slc = [slice(None)] * ndim
+            left_fine_slc = list(take_all_slice)
             left_fine_slc[i] = 0
             left_fine_parts.append(fine_arr[tuple(left_fine_slc)])
             right_fine_slc = [slice(None)] * ndim
@@ -181,10 +209,10 @@ class MultiGridGradient(odl.DiagonalOperator):
             aux_cell_sides[i] = coarse_cell_sides[i]
             print('aux_cell_sides:', aux_cell_sides)
 
-            left_min_pt = np.copy(ymin)
-            left_min_pt[i] = ymin_before[i]
-            left_max_pt = np.copy(ymax)
-            left_max_pt[i] = ymin_after[i]
+            left_min_pt = np.copy(yleft)
+            left_min_pt[i] = yleft_before[i]
+            left_max_pt = np.copy(yright)
+            left_max_pt[i] = yleft_after[i]
             aux_space_min = odl.uniform_discr_fromdiscr(
                 coarse_discr, min_pt=left_min_pt, max_pt=left_max_pt,
                 cell_sides=aux_cell_sides)
@@ -194,10 +222,10 @@ class MultiGridGradient(odl.DiagonalOperator):
             print('min space min/max:', aux_space_min.min_pt,
                   aux_space_min.max_pt)
 
-            right_min_pt = np.copy(ymin)
-            right_min_pt[i] = ymax_before[i]
-            right_max_pt = np.copy(ymax)
-            right_max_pt[i] = ymax_after[i]
+            right_min_pt = np.copy(yleft)
+            right_min_pt[i] = yright_before[i]
+            right_max_pt = np.copy(yright)
+            right_max_pt[i] = yright_after[i]
             aux_space_max = odl.uniform_discr_fromdiscr(
                 coarse_discr, min_pt=right_min_pt, max_pt=right_max_pt,
                 cell_sides=aux_cell_sides)
@@ -210,12 +238,12 @@ class MultiGridGradient(odl.DiagonalOperator):
             mg_slc = [None] * ndim
             mg_slc[i] = slice(None)
             left_meshgrid = list(fine_meshgrid)
-            left_meshgrid[i] = np.atleast_1d(ymin[i])[tuple(mg_slc)]
+            left_meshgrid[i] = np.atleast_1d(yleft[i])[tuple(mg_slc)]
             left_resampled_parts.append(aux_space_min.element(
                 coarse_func.interpolation(tuple(left_meshgrid))))
 
             right_meshgrid = list(fine_meshgrid)
-            right_meshgrid[i] = np.atleast_1d(ymax[i])[tuple(mg_slc)]
+            right_meshgrid[i] = np.atleast_1d(yright[i])[tuple(mg_slc)]
             right_resampled_parts.append(aux_space_max.element(
                 coarse_func.interpolation(tuple(right_meshgrid))))
 
